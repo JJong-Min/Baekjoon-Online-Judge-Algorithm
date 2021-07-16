6ed5c88157b0d49db768bcb4cbc231c259e38d6a
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-07-16 21:37:44 +0900

    Correct: 로그 파일 재정렬

---
+++ String(문자열)/로그 파일 재정렬.py
@@ -0,0 +1,15 @@
+def reorderLogFiles(logs):
+        letter = []
+        digit = []
+        for log in logs:
+                if log.split()[1].isdigit():
+                        digit.append(log)
+                else:
+                        letter.append(log)
+
+        letter = sorted(letter, key=lambda x : (x.split()[1:], x.split()[0]))
+        return letter+digit
+
+logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
+
+print(reorderLogFiles(logs))
